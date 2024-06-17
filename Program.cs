a5e959912a151dac89adf78fd4297a7386c50e9a
Mateo020528 <mateo.velez.censa@gmail.com>
2024-06-17 00:27:24 -0500

    Se corrigen el program

--- Program.cs
+++ Program.cs
@@ -1,22 +1,10 @@
 using Backengv2.Data;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using Microsoft.EntityFrameworkCore;
-<<<<<<< HEAD
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Backend.Services;
-=======
-using AutoMapper;
-using Microsoft.Extensions.DependencyInjection;
 using Backengv2.Profiles;
-using Backengv2.Services.Coupons;
-using Microsoft.AspNetCore.Mvc;
-
-using Backengv2.Services;
-
-
->>>>>>> 8897019b7930b8e922adcd7388608d7f50c8954f
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -35,7 +23,6 @@ builder.Services.AddDbContext<BaseContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("MySqlConnection"),
         Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.20-mysql")));
-<<<<<<< HEAD
 
 //Configuramos JsonWebToken
 builder.Services.AddAuthentication(opt => {
@@ -63,28 +50,6 @@ new AuthRepository(provider.GetRequiredService<BaseContext>(), "FTGUNMIMGI4MFI4J
 var app = builder.Build();
 app.UseAuthentication();  //Agregamos permisos para DataConection
 app.UseAuthorization();
-=======
-
-
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAnyOrigin",
-        builder => builder.AllowAnyOrigin()
-            .AllowAnyHeader()
-            .AllowAnyMethod());
-});
-  
-
-builder.Services.AddScoped<ICouponRepository, CouponRepository>();
-builder.Services.AddScoped<IMarketplaceUserRepository, MarketplaceUserRepository>();
-
-builder.Services.AddScoped<MailerSendService>();
-
-
-var app = builder.Build();
-app.UseMiddleware<ErrorHandlingMiddleware>();
->>>>>>> 8897019b7930b8e922adcd7388608d7f50c8954f
 app.MapControllers();
 
 // Configure the HTTP request pipeline.
